de537bdf6627f9673210d901b59adb52a375cbf6
Mati Andreas <mati.andreas@ambientia.ee>
2021-05-14 11:12:49 +0300

    add props -  validation ok

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
        @input="onInput"
      />
      <!-- UpperCase helps to distinguish between html extensions and Vue.js component -->
-     <BeautifulButton @click="onSubmit" :value="() => {}" />
+     <BeautifulButton @click="onSubmit" :value="'Submit'" />
      <div>
        <label>
          <input
